a0a1e7cf03368c534758ab52ab3f43588c6c4c6b
Ben Stein <bumstain@gmail.com>
2016-06-02 11:52:17 +0100

    User can add to-dos to the list

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -6,9 +6,19 @@ describe("app", function() {
 });
 
 describe('Todos tracker', function() {
-  it('has a todo', function() {
+  it('has several ToDos', function() {
     browser.get('/');
-    var todo = $('#todo');
-    expect(todo.getText()).toEqual('ToDo1');
+    var todos = $$('#todos p');
+    expect(todos.first().getText()).toEqual('ToDo1: completed');
+    expect(todos.last().getText()).toEqual('ToDo2: not completed');
   });
+
+  it('can add a ToDo', function() {
+    browser.get('/');
+    $('#new-todo-name').sendKeys("NewToDo"); 
+    $('#add-todo').click();
+
+    var todo = $$('#todos p').last().getText();
+    expect(todo).toEqual('NewToDo: not completed');
+  })
 });
